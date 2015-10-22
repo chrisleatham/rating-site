e24e73dd91160909ba76777725be1c2cc58076e5
Chris Leatham <chris13@uw.edu>
2015-10-21 21:56:12 -0700

    additions

---
+++ js/parse.js
@@ -0,0 +1,5 @@
+Parse.initialize("9w3zkAemBkVmgQjtHvVRudKNMNUGtCT94WnQrOYc", "SWCpPCOCyHWpoVwNyNfB1MY579rOL0BciLLVYv3U");
+
+var Review = Parse.Object.extend('review');
+
+var reviewItem = new Review
