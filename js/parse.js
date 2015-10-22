e4d8a1a899cb3ca6557577b1405d2029eec33d27
Chris Leatham <chris13@uw.edu>
2015-10-22 11:12:31 -0700

    fixed a bug and updated js

--- js/parse.js
+++ js/parse.js
@@ -1,5 +1,34 @@
 Parse.initialize("9w3zkAemBkVmgQjtHvVRudKNMNUGtCT94WnQrOYc", "SWCpPCOCyHWpoVwNyNfB1MY579rOL0BciLLVYv3U");
 
-var Review = Parse.Object.extend('review');
+var Review = Parse.Object.extend('Review');
 
-var reviewItem = new Review
+
+$('form').submit(function() {
+    
+    var reviewItem = new Review();
+
+    $(this).find('input').each(function(){
+        review.set($(this).attr('id'), $(this).val());
+        $(this).val('');
+    })
+
+    music.save(null,{
+        success: function() {
+            getData();
+        }
+    });
+
+    return false;
+)}
+
+var getData = function() {
+    var query = new Parse.Query(Review);
+    query.exists('title');
+    query.exists('review');
+
+    query.find({
+        success: function(response){
+            buildList(response);
+        }
+    });
+}
